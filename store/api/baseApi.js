754e3374046d433670eaa93ccd62a1fce390a779
arifjahan88 <arifjahan88@gmail.com>
2024-11-14 22:11:48 +0600

    fix:click to file delete

--- store/api/baseApi.js
+++ store/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api/v1",
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
   }),
 
   tagTypes: ["upload"],
